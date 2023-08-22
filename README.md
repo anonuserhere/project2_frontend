f0fbeb84af64d531c7e92ae30a7147e0549390e6
anonuserhere <131941537+anonuserhere@users.noreply.github.com>
2023-08-22 19:31:29 +0800

    updated readme and not_found page

--- README.md
+++ README.md
@@ -1,8 +1,41 @@
-Frontend for Project 2.
-Done in JavaScript.
+Frontend for Project 2 done in JavaScript.
+Hosted on Netlify (https://www.netlify.com)
+Have a look here: https://chic-pastelito-86f477.netlify.app/
+
+It is a simple representation of image posting sites such as Instagram and Imgur,
+where users can share a photo/picture/meme (a link in the case of this project)
+along with certain details.
+The objective was to create a clean and simple interface to post image media, and
+to view those contributed by others.
+Firebase is not implemented properly at the moment and I hope to circle back in future.
+That will give users the ability to access their own profile, along with being
+able to check their past posts.
 
 Technologies/libraries used:
+
 - react (with vite)
 - bootstrap
 - axios
 - firebase (unimplemented for now, as of 22/8/2023)
+
+Entity Relationship Diagram:
+![Alt text](public/project2_map.png)
+
+Users are able to view and edit (perform CRUD functions, i.e.
+create, read, update and delete) on posts, by navigating to the gallery and
+clicking on specific posts.
+
+![Alt text](public/buttons.png)
+
+Users are also able to search for posts by tags (more functionality to be added in future,
+for example, date added, within captions, etc) for sorting purposes in the search field right
+at the top of the gallery page.
+
+Four months ago, if you told me I would be able to build something like this,
+however rudimentary, I would not have believed you. Yet here in the later half of 2023,
+we have the technology and tools available to build it, almost all of which are readily
+available free-of-charge and maintained by the community, with instructions on how to do so
+to boot.
+
+Many thanks to the community for the gracious use of these tools and also teachers both online and off. Special shout-out to Edison for the file layout/organization tips and YS for guiding
+through deployment.
--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -38,71 +38,75 @@ export function Gallery() {
     navigate(`/gallery/${postId}`);
   };
 
-  return (
-    <>
-      <div className="gallery-container d-flex-col mt-3">
-        <form
-          className="form d-flex justify-content-center"
-          onSubmit={handleSearch}
-        >
-          <div
-            className="input-group mb-3"
-            style={{ width: "60dvw", height: "50px" }}
+  if (images === null || images.length === 0) {
+    navigate("/not_found");
+  } else {
+    return (
+      <>
+        <div className="gallery-container d-flex-col mt-3">
+          <form
+            className="form d-flex justify-content-center"
+            onSubmit={handleSearch}
           >
-            <input
-              name="search"
-              id="search"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-              type="text"
-              className="form-control"
-              placeholder="Search by tags"
-              aria-label="search input"
-              aria-describedby="basic-addon2"
-            />
-            <div className="input-group-append">
-              <button
-                className="btn btn-info"
-                style={{ height: "50px", borderRadius: "3px" }}
-                type="submit"
-              >
-                Search
-              </button>
-            </div>
-          </div>
-        </form>
-        <div
-          className="card-container d-grid justify-content-center grid-template-columns m-4 p-0"
-          style={{
-            gridTemplateColumns: "repeat(auto-fill, minmax(300px, 3fr)",
-          }}
-        >
-          {images.map((i) => {
-            return (
-              <div
-                className="card m-1"
-                id="image_card"
-                key={i._id}
-                onClick={() => handleSelection(i._id)}
-                style={{ height: "45dvh" }}
-              >
-                <img
-                  src={i.url}
-                  className="card-img-top"
-                  style={{ height: "80%" }}
-                  alt="pic"
-                />
-                <div
-                  className="card-body"
-                  style={{ color: "darkblue", backgroundColor: "cornsilk" }}
+            <div
+              className="input-group mb-3"
+              style={{ width: "60dvw", height: "50px" }}
+            >
+              <input
+                name="search"
+                id="search"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                type="text"
+                className="form-control"
+                placeholder="Search by tags"
+                aria-label="search input"
+                aria-describedby="basic-addon2"
+              />
+              <div className="input-group-append">
+                <button
+                  className="btn btn-info"
+                  style={{ height: "50px", borderRadius: "3px" }}
+                  type="submit"
                 >
-                  <p className="card-text">Posted by: {i.user}</p>
-                </div>
+                  Search
+                </button>
               </div>
-            );
-          })}
+            </div>
+          </form>
+          <div
+            className="card-container d-grid justify-content-center grid-template-columns m-4 p-0"
+            style={{
+              gridTemplateColumns: "repeat(auto-fill, minmax(300px, 3fr)",
+            }}
+          >
+            {images.map((i) => {
+              return (
+                <div
+                  className="card m-1"
+                  id="image_card"
+                  key={i._id}
+                  onClick={() => handleSelection(i._id)}
+                  style={{ height: "45dvh" }}
+                >
+                  <img
+                    src={i.url}
+                    className="card-img-top"
+                    style={{ height: "80%" }}
+                    alt="pic"
+                  />
+                  <div
+                    className="card-body"
+                    style={{ color: "darkblue", backgroundColor: "cornsilk" }}
+                  >
+                    <p className="card-text">Posted by: {i.user}</p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
